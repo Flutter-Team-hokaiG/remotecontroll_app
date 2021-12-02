d31ae7c4f614134e2e99eccbc92981260d229fb1
rion15007 <93176562+rion15007@users.noreply.github.com>
2021-12-02 14:32:22 +0900

    PageViewの導入

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,73 @@
-//import 'dart:io';
-
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  static const String _title = 'Flutter Code Sample';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'aaaa',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(
-        title: 'DEMO Page',
-      ),
+      title: _title,
+      home: MyWidget(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
+class MyWidget extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyWidgetState createState() => _MyWidgetState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyWidgetState extends State<MyWidget> {
+  late PageController controller;
+
+  @override
+  void initState() {
+    controller = PageController();
+    super.initState();
+  }
+
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: (ElevatedButton(
-          child: Text('A'),
-          onPressed: () {},
-        )),
+      appBar: AppBar(
+        leading: Icon(Icons.dangerous),
+        title: const Text('AppBar'),
+      ),
+      body: PageView(
+        controller: controller,
+        children: <Widget>[
+          page1(),
+          page2(),
+        ],
+      ),
+    );
+  }
+
+  Widget page1() {
+    return Center(
+      child: Container(
+        child: Text(
+          'PageView(横)',
+          style: TextStyle(fontSize: 50.0),
+        ),
+      ),
+    );
+  }
+
+  Widget page2() {
+    return Center(
+      child: Container(
+        child: Text(
+          '成功',
+          style: TextStyle(fontSize: 30.0),
+        ),
       ),
     );
   }
