48aa47126aff0562520f484fe8fd39fd6cb18e49
rion15007 <93176562+rion15007@users.noreply.github.com>
2022-01-20 14:50:57 +0900

    GridViewを追加

--- lib/main.dart
+++ lib/main.dart
@@ -38,13 +38,172 @@ class _MyWidgetState extends State<MyWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Icon(Icons.dangerous),
-        title: const Text('AppBar'),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(20),
+        child: AppBar(
+          backgroundColor: Colors.red,
+          automaticallyImplyLeading: false,
+          actions: const [],
+          elevation: 8,
+        ),
       ),
       body: Column(
         children: [
-          Text('dataA', textScaleFactor: 4),
+          Padding(
+            padding: const EdgeInsetsDirectional.fromSTEB(2, 2, 2, 2),
+            child: GridView(
+              padding: EdgeInsets.zero,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                crossAxisSpacing: 1,
+                mainAxisSpacing: 1,
+                childAspectRatio: 2,
+              ),
+              primary: false,
+              shrinkWrap: true,
+              scrollDirection: Axis.vertical,
+              children: [
+                Stack(
+                  children: [
+                    Align(
+                      alignment: const AlignmentDirectional(0, -0.5),
+                      child: IconButton(
+                        iconSize: 50,
+                        icon: const Icon(
+                          Icons.settings,
+                          color: Colors.black,
+                          size: 20,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                    const Align(
+                      alignment: AlignmentDirectional(0, 0.8),
+                      child: Text(
+                        '通常',
+                        style: TextStyle(fontWeight: FontWeight.w800),
+                      ),
+                    ),
+                  ],
+                ),
+                Stack(
+                  children: [
+                    Align(
+                      alignment: const AlignmentDirectional(0, -0.5),
+                      child: IconButton(
+                        iconSize: 50,
+                        icon: const Icon(
+                          Icons.directions_car,
+                          color: Colors.black,
+                          size: 20,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                    const Align(
+                      alignment: AlignmentDirectional(0, 0.8),
+                      child: Text(
+                        '騒がしい',
+                        style: TextStyle(fontWeight: FontWeight.w800),
+                      ),
+                    ),
+                  ],
+                ),
+                Stack(
+                  children: [
+                    Align(
+                      alignment: const AlignmentDirectional(0, -0.5),
+                      child: IconButton(
+                        iconSize: 50,
+                        icon: const Icon(
+                          Icons.piano_off,
+                          color: Colors.black,
+                          size: 20,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                    const Align(
+                      alignment: AlignmentDirectional(0, 0.8),
+                      child: Text(
+                        '静か',
+                        style: TextStyle(fontWeight: FontWeight.w800),
+                      ),
+                    ),
+                  ],
+                ),
+                Stack(
+                  children: [
+                    Align(
+                      alignment: const AlignmentDirectional(0, -0.5),
+                      child: IconButton(
+                        iconSize: 50,
+                        icon: const Icon(
+                          Icons.local_restaurant,
+                          color: Colors.black,
+                          size: 20,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                    const Align(
+                      alignment: AlignmentDirectional(0, 0.8),
+                      child: Text(
+                        'レストラン',
+                        style: TextStyle(fontWeight: FontWeight.w800),
+                      ),
+                    ),
+                  ],
+                ),
+                Stack(
+                  children: [
+                    Align(
+                      alignment: const AlignmentDirectional(0, -0.5),
+                      child: IconButton(
+                        iconSize: 50,
+                        icon: const Icon(
+                          Icons.record_voice_over,
+                          color: Colors.black,
+                          size: 20,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                    const Align(
+                      alignment: AlignmentDirectional(0, 0.8),
+                      child: Text(
+                        '会話',
+                        style: TextStyle(fontWeight: FontWeight.w800),
+                      ),
+                    ),
+                  ],
+                ),
+                Stack(
+                  children: [
+                    Align(
+                      alignment: const AlignmentDirectional(0, -0.5),
+                      child: IconButton(
+                        iconSize: 50,
+                        icon: const Icon(
+                          Icons.phone_in_talk,
+                          color: Colors.black,
+                          size: 20,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                    const Align(
+                      alignment: AlignmentDirectional(0, 0.8),
+                      child: Text(
+                        '電話',
+                        style: TextStyle(fontWeight: FontWeight.w800),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
           Expanded(
             child: PageView(
               controller: controller,
