5a7353c0a725a0e58b17a77e8e85455c7fdbc488
rion15007 <93176562+rion15007@users.noreply.github.com>
2022-03-11 14:08:40 +0900

    「元に戻す」ボタンを一度押すとsliderValueとinitialValueが連動してしまい、二回目から使えなくなってしまったため、それも修正

--- lib/main.dart
+++ lib/main.dart
@@ -26,8 +26,8 @@ class MyWidget extends StatefulWidget {
 class _MyWidgetState extends State<MyWidget> {
   late PageController controller;
   int _memory = 0;
-  List<int> initialValue1 = [16, 16, 16, 16, 16, 16]; // [元に戻す]の値
-  List<int> initialValue2 = [16, 16, 16, 16, 16, 16]; // [元に戻す]の値
+  // List<int> initialValue1 = [16, 16, 16, 16, 16, 16]; // [元に戻す]の値
+  // List<int> initialValue2 = [16, 16, 16, 16, 16, 16]; // [元に戻す]の値
   List<int> sliderValue1 = [16, 16, 16, 16, 16, 16]; // メインボリューム左
   List<int> sliderValue2 = [16, 16, 16, 16, 16, 16]; // メインボリューム右
   int _volmax = 32; // メインボリューム最大
@@ -416,8 +416,8 @@ class _MyWidgetState extends State<MyWidget> {
                                 ElevatedButton(
                                   onPressed: () {
                                     setState(() {
-                                      sliderValue1 = initialValue1;
-                                      sliderValue2 = initialValue2;
+                                      sliderValue1 = [16, 16, 16, 16, 16, 16];
+                                      sliderValue2 = [16, 16, 16, 16, 16, 16];
                                     });
                                   },
                                   child: Text('元に戻す'),
